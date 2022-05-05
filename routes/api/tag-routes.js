90bf4326afddfbe34d929fc83fc894cb2083034f
coderbennett <jbennett@csumb.edu>
2022-05-04 19:42:09 -0700

    add tag delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -51,7 +51,7 @@ router.put('/:id', async (req, res) => {
     });
 
     if(!tagData) {
-      res.status(404).json({ message: 'No tag with this id'});
+      res.status(404).json({ message: 'No tag with this id found'});
       return;
     }
     res.status(200).json(tagData);
@@ -60,8 +60,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({message: 'No tag with this id found'})
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
